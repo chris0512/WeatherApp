51ec71e562ccfa1ea2d431d2c3eaefb6f46529bc
chris0512 <kyang3200@gmail.com>
2022-08-01 13:23:59 +0800

    Initial Commit

---
+++ WeatherApp/Views/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Yang Chi on 2022/7/31.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct ContentView: View {
+    
+    
+    // initialize our LocationManager
+    @StateObject var locationManager = LocationManager()
+    
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
+    
+    var body: some View {
+        VStack {
+            
+            if let location = locationManager.location {
+                if let weather = weather {
+                    WeatherView(weather: weather)
+                } else {
+                    LoadingView()
+                        .task {
+                            do {
+                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print("Error getting weawther:\(error)")
+                            }
+                            
+                        }
+                }
+            } else {
+                if locationManager.isLoading {
+                    LoadingView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+            
+            
+            
+           
+        }
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Yang Chi on 2022/7/31.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
